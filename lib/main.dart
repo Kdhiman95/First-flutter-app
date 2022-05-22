d4ab32c7352037d4547ff28ae85c5fce79ddc538
Kamal Dhiman <kamal.dhiman2001@gmail.com>
2022-05-22 18:03:22 +0530

    clear import statement of colors.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
-import 'package:flutter/src/material/colors.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
